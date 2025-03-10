3767877c70a7d70c20045e6d7549fd6906db1c0d
Faf069 <faridh.badaoui@gmail.com>
2025-03-10 11:32:18 +0100

    header login

--- pages/home.js
+++ pages/home.js
@@ -1,13 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import HeaderUser from "../components/HeaderUser";
+import HeaderTechnicien from "../components/HeaderTechnicien";
+import HeaderAdministrateur from "../components/HeaderAdministrateur";
 import DemandeTickets from "../components/DemandeTickets";
 import LastTickets from "../components/LastTickets";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem("role"); // ✅ Récupère le rôle stocké
+    if (storedRole) {
+      setUserRole(storedRole);
+    }
+  }, []);
+
   return (
     <div className={styles.main}>
-      <HeaderUser />
+      {/* ✅ Affichage dynamique du bon Header */}
+      {userRole === "Administrateur" ? (
+        <HeaderAdministrateur />
+      ) : userRole === "Technicien" ? (
+        <HeaderTechnicien />
+      ) : (
+        <HeaderUser />
+      )}
+
       <div className={styles.container}>
         <DemandeTickets />
         <LastTickets />
